3ef701f02e7ac5af90efbeb4dbb042b23e38d460
unknown <gfram@hotmail.com>
2019-09-29 11:40:31 +0100

    first commit

---
+++ DemoAPI/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoAPI.Models
+{
+    public class Person
+    {
+        //Manually increments the value
+        public int Id { get; set; } = 0; 
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+    }
+}
